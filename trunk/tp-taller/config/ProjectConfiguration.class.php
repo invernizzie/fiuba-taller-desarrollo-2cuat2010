d4d52211c2f05f2f18f1ceccc6f6786f0cf65dad
invernizzie <invernizzie@b062089f-8c74-b0f7-cfd7-d079cf4a93fc>
2010-09-28 21:11:35 +0000

    Instalo plugins sfDoctrineGuardPlugin y sfFacebookConnectPlugin

--- trunk/tp-taller/config/ProjectConfiguration.class.php
+++ trunk/tp-taller/config/ProjectConfiguration.class.php
@@ -11,6 +11,10 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins(array(
+      'sfDoctrinePlugin',
+      'sfDoctrineGuardPlugin',
+      'sfFacebookConnectPlugin'
+    ));
   }
 }
