f4e9329d3478023673525a196f954cc0b0f9706f
invernizzie <invernizzie@b062089f-8c74-b0f7-cfd7-d079cf4a93fc>
2010-10-02 18:55:25 +0000

    Agrego la configuracion de Facebook Connect

---
+++ tp-taller/apps/frontend/templates/layout_connect.php
@@ -0,0 +1,18 @@
+<?php use_helper('sfFacebookConnect') ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <script type="text/javascript" src="/sfFacebookConnectPlugin/js/animation/animation.js"></script>
+  </head>
+  <body>
+    
+    <?php $sfGuardUser = sfFacebook::getSfGuardUserByFacebookSession(); ?>
+    Hello <fb:name uid="<?php echo $sfGuardUser ? $sfGuardUser->getProfile()->getFacebookUid() : '' ?>"></fb:name>
+
+    <?php echo $sf_content ?>
+    <?php echo include_facebook_connect_script() ?>
+  </body>
+</html>
