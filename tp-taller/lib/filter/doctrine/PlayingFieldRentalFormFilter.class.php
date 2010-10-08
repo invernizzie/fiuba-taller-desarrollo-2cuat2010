2ecf32ff3bcb5b11193ca505110706a5b603170f
invernizzie <invernizzie@b062089f-8c74-b0f7-cfd7-d079cf4a93fc>
2010-10-08 22:51:01 +0000

    Subo el modelo generado por Doctrine

---
+++ tp-taller/lib/filter/doctrine/PlayingFieldRentalFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PlayingFieldRental filter form.
+ *
+ * @package    tp-taller
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PlayingFieldRentalFormFilter extends BasePlayingFieldRentalFormFilter
+{
+  public function configure()
+  {
+  }
+}
