3e0cc2a1310e653c6777933a7fae378e72359764
invernizzie <invernizzie@b062089f-8c74-b0f7-cfd7-d079cf4a93fc>
2010-10-08 22:55:26 +0000

    Subo SQL y forms generados por Doctrine

---
+++ trunk/tp-taller/lib/form/doctrine/base/BaseMatchForm.class.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Match form base class.
+ *
+ * @method Match getObject() Returns the current form's model object
+ *
+ * @package    tp-taller
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseMatchForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'            => new sfWidgetFormInputHidden(),
+      'starting_time' => new sfWidgetFormDateTime(),
+      'closed'        => new sfWidgetFormInputCheckbox(),
+      'played'        => new sfWidgetFormInputCheckbox(),
+      'public'        => new sfWidgetFormInputCheckbox(),
+      'discipline_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Discipline'), 'add_empty' => false)),
+      'owner_id'      => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Owner'), 'add_empty' => true)),
+      'rental_id'     => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Court'), 'add_empty' => true)),
+      'local_id'      => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('LocalTeam'), 'add_empty' => true)),
+      'away_id'       => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('AwayTeam'), 'add_empty' => true)),
+      'created_at'    => new sfWidgetFormDateTime(),
+      'updated_at'    => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'            => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'starting_time' => new sfValidatorDateTime(array('required' => false)),
+      'closed'        => new sfValidatorBoolean(array('required' => false)),
+      'played'        => new sfValidatorBoolean(array('required' => false)),
+      'public'        => new sfValidatorBoolean(array('required' => false)),
+      'discipline_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Discipline'))),
+      'owner_id'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Owner'), 'required' => false)),
+      'rental_id'     => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Court'), 'required' => false)),
+      'local_id'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('LocalTeam'), 'required' => false)),
+      'away_id'       => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('AwayTeam'), 'required' => false)),
+      'created_at'    => new sfValidatorDateTime(),
+      'updated_at'    => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('match[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Match';
+  }
+
+}
---
+++ trunk/tp-taller/lib/form/doctrine/base/BasePlayingFieldRentalForm.class.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * PlayingFieldRental form base class.
+ *
+ * @method PlayingFieldRental getObject() Returns the current form's model object
+ *
+ * @package    tp-taller
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BasePlayingFieldRentalForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'               => new sfWidgetFormInputHidden(),
+      'name'             => new sfWidgetFormInputText(),
+      'address'          => new sfWidgetFormInputText(),
+      'city'             => new sfWidgetFormInputText(),
+      'province'         => new sfWidgetFormInputText(),
+      'country'          => new sfWidgetFormInputText(),
+      'phoneNo'          => new sfWidgetFormInputText(),
+      'created_at'       => new sfWidgetFormDateTime(),
+      'updated_at'       => new sfWidgetFormDateTime(),
+      'disciplines_list' => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'Discipline')),
+    ));
+
+    $this->setValidators(array(
+      'id'               => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'name'             => new sfValidatorString(array('max_length' => 255)),
+      'address'          => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'city'             => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'province'         => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'country'          => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'phoneNo'          => new sfValidatorString(array('max_length' => 40, 'required' => false)),
+      'created_at'       => new sfValidatorDateTime(),
+      'updated_at'       => new sfValidatorDateTime(),
+      'disciplines_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'Discipline', 'required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('playing_field_rental[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'PlayingFieldRental';
+  }
+
+  public function updateDefaultsFromObject()
+  {
+    parent::updateDefaultsFromObject();
+
+    if (isset($this->widgetSchema['disciplines_list']))
+    {
+      $this->setDefault('disciplines_list', $this->object->Disciplines->getPrimaryKeys());
+    }
+
+  }
+
+  protected function doSave($con = null)
+  {
+    $this->saveDisciplinesList($con);
+
+    parent::doSave($con);
+  }
+
+  public function saveDisciplinesList($con = null)
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    if (!isset($this->widgetSchema['disciplines_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (null === $con)
+    {
+      $con = $this->getConnection();
+    }
+
+    $existing = $this->object->Disciplines->getPrimaryKeys();
+    $values = $this->getValue('disciplines_list');
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('Disciplines', array_values($unlink));
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('Disciplines', array_values($link));
+    }
+  }
+
+}
