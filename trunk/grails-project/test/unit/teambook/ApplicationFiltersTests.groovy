faee7f1d9e50182505971a54ebcc9e062d81fa62
epidemian <epidemian@b062089f-8c74-b0f7-cfd7-d079cf4a93fc>
2011-03-02 12:59:05 +0000

    Incoming matches en el panel lateral. Lo implementé con un "filter", que se ejecuta en cada request... es un toque "overkill" por ahí si tuviesemos mucho tráfico, pero es lo más facil de implementar que se me ocurrió para tener una variable "incomingMatches" en cualquier pantalla y poder dibujar la listita del costado.

---
+++ trunk/grails-project/test/unit/teambook/ApplicationFiltersTests.groovy
@@ -0,0 +1,17 @@
+package teambook
+
+import grails.test.*
+
+class ApplicationFiltersTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
